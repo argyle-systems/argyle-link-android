3df1c794027aab175efdeae231182c7feb387d35
Adrian Estran <adrian@argyle.com>
2022-06-24 11:09:40 +0100

    Update the readme and overhaul the Example App implementation

--- app/src/main/java/com/argyleexample/MainActivity.kt
+++ app/src/main/java/com/argyleexample/MainActivity.kt
@@ -1,20 +1,16 @@
 package com.argyleexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
-import android.util.Log
-import android.widget.Button
-
-import com.argyle.ArgyleConfig
-
 import com.argyle.Argyle
+import com.argyle.ArgyleConfig
 import com.argyle.ArgyleErrorType
+import com.argyleexample.databinding.MainActivityBinding
 
-private const val TAG = "MainActivity"
-
-private const val LINK_KEY = "YOUR_LINK_KEY"
-private const val API_HOST = "https://api-sandbox.argyle.com/v1"
+private const val LINK_KEY = "017cf0de-aac1-e1c4-b86c-7d9dffbfc8ed"
+private const val API_HOST = "https://api-sandbox.argyle.com/v1/"
 
 //  https://argyle.com/docs/pay-distributions-guide/link-integration
 private const val YOUR_PD_CONFIG = "YOUR_PD_CONFIG"
@@ -22,123 +18,105 @@ private const val YOUR_PD_CONFIG = "YOUR_PD_CONFIG"
 private const val PREF_USER_TOKEN = "userToken"
 private const val PREF_USER_ID = "userId"
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Argyle.ArgyleResultListener {
+
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
 
-        configureArgyleSdk(null)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val newWorkerButton = findViewById<Button>(R.id.newWorkerButton)
-        newWorkerButton?.setOnClickListener {
-            Argyle.instance.config.userToken = null // nullify user token
-            openSdk()
+        binding.newWorkerButton.setOnClickListener {
+            startLink(returning_user_token = null)
         }
 
-        val existingButton = findViewById<Button>(R.id.existingButton)
-        existingButton?.setOnClickListener {
+        binding.existingButton.setOnClickListener {
             val preferences = PreferenceManager.getDefaultSharedPreferences(this)
             val token = preferences.getString(PREF_USER_TOKEN, null)
-            Argyle.instance.config.userToken = token // change user token
-            openSdk()
+
+            if (token == null) {
+                Toast.makeText(this@MainActivity, "Create a user first", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            startLink(returning_user_token = token)
         }
     }
 
-    private fun openSdk() {
+    private fun startLink(returning_user_token: String?) {
+        val linkConfig = ArgyleConfig.Builder().loginWith(LINK_KEY, API_HOST)
+            .setCallbackListener(this).build()
+
+        returning_user_token?.let {
+            linkConfig.userToken = it
+        } ?: run { linkConfig.userToken = null }
+
+        Argyle.instance.init(linkConfig)
         Argyle.instance.startSDK(this)
     }
 
-    private fun configureArgyleSdk(token: String? = null) {
-        val argyle = Argyle.instance
+    override fun onTokenExpired(handler: (String) -> Unit) {
+        val token = "token"
+        handler(token)
+    }
 
-        Log.d(TAG, "openSdk with token : $token")
+    override fun onAccountCreated(accountId: String, userId: String, linkItemId: String) {
+        Toast.makeText(this@MainActivity, "onAccountCreated", Toast.LENGTH_SHORT).show()
+    }
 
-        val config = ArgyleConfig.Builder()
+    override fun onAccountConnected(accountId: String, userId: String, linkItemId: String) {
+        Toast.makeText(this@MainActivity, "onAccountConnected", Toast.LENGTH_SHORT).show()
+    }
 
-            .loginWith(LINK_KEY, API_HOST, token)
-//            .linkItems(arrayOf("uber"))
-//            .payDistributionConfig(YOUR_PD_CONFIG)
-//            .payDistributionItemsOnly(true)
-//            .payDistributionUpdateFlow(true)
-            .setCallbackListener(object : Argyle.ArgyleResultListener {
+    override fun onAccountUpdated(accountId: String, userId: String, linkItemId: String) {
+        Toast.makeText(this@MainActivity, "onAccountUpdated", Toast.LENGTH_SHORT).show()
+    }
 
-                override fun onTokenExpired(handler: (String) -> Unit) {
-                    val token = "token"
-                    handler(token)
-                }
+    override fun onAccountRemoved(accountId: String, userId: String, linkItemId: String) {
+        Toast.makeText(this@MainActivity, "onAccountRemoved", Toast.LENGTH_SHORT).show()
+    }
 
-                override fun onAccountCreated(accountId: String, userId: String, linkItemId:String) {
-                    Log.d(TAG, "onAccountCreated: accountId: $accountId workerId: $userId linkItemId: $linkItemId")
-                }
+    override fun onAccountError(accountId: String, userId: String, linkItemId: String) {
+        Toast.makeText(this@MainActivity, "onAccountError", Toast.LENGTH_SHORT).show()
+    }
 
-                override fun onAccountConnected(accountId: String, userId: String, linkItemId:String) {
-                    Log.d(TAG, "onAccountConnected: accountId: $accountId workerId: $userId linkItemId: $linkItemId")
-                }
+    override fun onError(error: ArgyleErrorType) {
+        Toast.makeText(this@MainActivity, "onError", Toast.LENGTH_SHORT).show()
+    }
 
-                override fun onAccountUpdated(accountId: String, userId: String, linkItemId:String) {
-                    Log.d(TAG, "onAccountUpdated: accountId: $accountId workerId: $userId linkItemId: $linkItemId")
-                }
+    override fun onUserCreated(userToken: String, userId: String) {
+        PreferenceManager.getDefaultSharedPreferences(applicationContext).edit().apply {
+            putString(PREF_USER_TOKEN, userToken)
+            putString(PREF_USER_ID, userId)
+            apply()
+            Toast.makeText(this@MainActivity, "onUserCreated", Toast.LENGTH_SHORT).show()
+        }
+    }
 
-                override fun onAccountRemoved(accountId: String, userId: String, linkItemId:String) {
-                    Log.d(TAG, "onAccountRemoved: accountId: $accountId workerId: $userId linkItemId: $linkItemId")
-                }
+    override fun onClose() {
+        Toast.makeText(this@MainActivity, "onClose", Toast.LENGTH_SHORT).show()
+    }
 
-                override fun onAccountError(accountId: String, userId: String, linkItemId: String
-                ) {
-                    Log.d(TAG, "onAccountError: accountId: $accountId workerId: $userId linkItemId: $linkItemId")
-                }
+    override fun onPayDistributionError(
+        accountId: String,
+        userId: String,
+        linkItemId: String
+    ) {
+        Toast.makeText(this@MainActivity, "onPayDistributionError", Toast.LENGTH_SHORT).show()
+    }
 
-                override fun onError(error: ArgyleErrorType) {
-                    Log.d(TAG, "onError: error: $error")
-                }
+    override fun onPayDistributionSuccess(
+        accountId: String,
+        userId: String,
+        linkItemId: String
+    ) {
+        Toast.makeText(this@MainActivity, "onPayDistributionSuccess", Toast.LENGTH_SHORT).show()
+    }
 
-                override fun onUserCreated(userToken: String, userId: String) {
-                    val preferences =
-                        PreferenceManager.getDefaultSharedPreferences(applicationContext)
-                    val editor = preferences.edit()
-                    editor.putString(PREF_USER_TOKEN, userToken)
-                    editor.putString(PREF_USER_ID, userId)
-                    editor.apply()
-                    Log.d(
-                        TAG,
-                        "onUserCreated:  userId: $userId userToken: $userToken"
-                    )
-                }
-
-                override fun onClose() {
-                    Log.d(TAG, "onClose")
-                }
-
-                override fun onPayDistributionError(
-                    accountId: String,
-                    userId: String,
-                    linkItemId: String
-                ) {
-                    Log.d(
-                        TAG,
-                        "onPayDistributionError: accountId: $accountId userId: $userId linkItemId: $linkItemId"
-                    )
-                }
-
-                override fun onPayDistributionSuccess(
-                    accountId: String,
-                    userId: String,
-                    linkItemId: String
-                ) {
-                    Log.d(
-                        TAG,
-                        "onPayDistributionSuccess: accountId: $accountId userId: $userId linkItemId: $linkItemId"
-                    )
-                }
-
-                override fun onUIEvent(name: String, properties: Map<String, Any>) {
-                    Log.d(TAG, "onUIEvent: $name, properties: $properties")
-                }
-
-            })
-            .build()
-
-        argyle.init(config)
+    override fun onUIEvent(name: String, properties: Map<String, Any>) {
+        Toast.makeText(this@MainActivity, "onUIEvent", Toast.LENGTH_SHORT).show()
     }
 
 }
